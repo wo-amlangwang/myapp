35721fd7322e62f4b3c7dca46e0c5494709d76a9
wo-amlangwang <adamlangwang@gmail.com>
2015-10-31 17:15:52 -0400

    second commit

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 var request = require('request');
-var url = 'http://api.purdue.io/odata/Courses?%24filter=Subject/Abbreviation%20eq%20%27CS%27&%24orderby=Number%20asc';
+var fs = require('fs');
+var url,filename;
+var abb = require('./abbreviation.json');
+var Promise = require('promise');
 
+for (var a in abb.abbrevation){
+	console.log(abb.abbrevation[a]);
+
+url = 'http://api.purdue.io/odata/Courses?$filter=Subject/Abbreviation eq \''+ abb.abbrevation[a] +'\'&$orderby=Number asc'
+filename = './' + abb.abbrevation[a] + '.json';
 request({url : url,
 	 json : true},function(err,response,body){
 		if(!err && response.statusCode === 200){
+			/*
+			console.log('=====================');
 			console.log(body);
+			console.log('=====================');
+			*/
+			fs.writeFile(filename,JSON.stringify(body),function(err){
+				if(err){
+					console.log(err);
+				}
+				console.log(filename + 'done!\n' );
+			});
 		}
 	});
 
+}
+
+
+var callme = function(abbre){
+	var ps = new promise()
+}
 
