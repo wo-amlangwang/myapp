3fa37e719436929fbcc1146fc4dc1473023daa6d
wo-amlangwang <adamlangwang@gmail.com>
2015-11-21 17:07:02 -0500

    update

--- app.js
+++ app.js
@@ -19,6 +19,18 @@ var callme = function(abbre){
 				if(err){
 					reject(err);
 				}
+				var m = JSON.parse(fs.readFileSync(filename).toString());
+				
+				for(var a in m.value){
+					m.value[a]['Abbreviation'] = abbre;
+				}
+				fs.writeFile(filename, JSON.stringify(m));
+				/*
+				m.forEach(function(p){
+    				p.pic = abbre;
+				});
+				fs.writeFile(filename, JSON.stringify(m));
+	*/
 				resolved(abbre);
 			});
 			}
@@ -39,6 +51,28 @@ var getabbrevation = function() {
 	return ps;
 }
 
+var mergeeverythingintoonefile = function(abbre) {
+	var ps = new Promise(function(resolved,reject){
+		var mergefile = './Everything.json';
+		var filename = './' + abbre + '.json';
+		fs.stat(mergefile,function(err,stat) {
+			if(err == null) {
+				console.log(success('find file and keep doning\n'));
+			}else{
+				var buffer = {'result' : '[]'};
+				fs.writeFile(mergefile, JSON.stringify(buffer));
+			}
+			var m = JSON.parse(fs.readFileSync(mergefile).toString());
+			var o = JSON.parse(fs.readFileSync(filename).toString());
+			for(var a in o.value){
+				m['result'].push(o.value[a]);				
+			}
+			resolved(abbre);
+		});
+	});
+	return ps;
+}
+
 
 getabbrevation().then(function (argument) {
 	//console.log(argument.value);
@@ -46,9 +80,13 @@ getabbrevation().then(function (argument) {
 		//console.log(a);
 		callme(argument.value[a]['Abbreviation']).then(function(msg){
 			console.log(success(msg  + ' success!\n'));
+			mergeeverythingintoonefile(argument.value[a]['Abbreviation']).then(function(message){
+				console.log(success(message + ' merged!'));
+			});
 		}).catch(function(err){
 			console.log(error(err));
 		});
+		
 	}
 });
 
